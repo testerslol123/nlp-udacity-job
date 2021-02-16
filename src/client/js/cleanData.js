f9ccd75c44a39d3708d62e81fd9c4273b1fab646
Eduardo Lopez <elopez@avascent.com>
2021-02-16 07:20:15 -0500

    SASS files added

--- src/client/js/cleanData.js
+++ src/client/js/cleanData.js
@@ -1,5 +1,10 @@
 function cleanData(data) {
 
+    const irony = document.getElementById('irony');
+    const confidence = document.getElementById('confidence'); 
+    const agreement = document.getElementById('agreement'); 
+    const subjectivity = document.getElementById('subjectivity');
+
     irony.textContent = `IRONY: ${data.irony}`;
     confidence.textContent = `CONFIDENCE: ${data.confidence}`;
     agreement.textContent = `AGREEMENT: ${data.agreement}`;
--- src/client/js/handleClick.js
+++ src/client/js/handleClick.js
@@ -3,13 +3,9 @@ import {callAPI} from "./callAPI"
 
 function handleClick()
     {
-        const btn = document.getElementById('btn');
         const url = document.getElementById('url');
         
-        const irony = document.getElementById('irony');
-        const confidence = document.getElementById('confidence'); 
-        const agreement = document.getElementById('agreement'); 
-        const subjectivity = document.getElementById('subjectivity'); 
+ 
         
         console.log('button pressed')
         
