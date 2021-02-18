d59276d8d173882445c4149430563edb5266536e
Eduardo Lopez <elopez@avascent.com>
2021-02-17 20:50:03 -0500

    dev server unable to reach the server.js file

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 mode: "development",
@@ -17,6 +17,7 @@ output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
 },
+
 module: {
     rules: [
        {
@@ -39,7 +40,16 @@ plugins: [
     new HtmlWebPackPlugin({
         template: "./src/client/views/index.html",
         filename: "./index.html"
-    })
+    }),
+    new CleanWebpackPlugin({
+        // Simulate the removal of files
+        dry: true,
+        // Write Logs to Console
+        verbose: true,
+        // Automatically remove all unused webpack assets on rebuild
+        cleanStaleWebpackAssets: true,
+        protectWebpackAssets: false
+})
 ]
 
 };
